3719bb90af17097fa184db7d6f0270155e5c7e4c
bajoelabrigo <bajoelabrigo@gmail.com>
2024-10-28 11:42:59 -0500

    create pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Project from "./pages/Project";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
-    <div className="text-3xl text-red-500">App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
