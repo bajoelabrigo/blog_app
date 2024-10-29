cfa992f61ddbfdfb80c34844f49f167e5570ea55
bajoelabrigo <bajoelabrigo@gmail.com>
2024-10-29 16:39:14 -0500

    footer ui finish

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from "./pages/SignIn";
 import Project from "./pages/Project";
 import Dashboard from "./pages/Dashboard";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/projects" element={<Project />} as={"div"}/>
         <Route path="/dashboard" element={<Dashboard />} as={"div"}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
